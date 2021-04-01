43b0e6531ea68c41a3db7c3b1afc5304789f48b5
Hunter Humburg <hunter.humburg@gmail.com>
2021-04-01 19:18:44 -0400

    Add links to comments for easy bookmarks to continue to work on this

--- seatmap_parser.py
+++ seatmap_parser.py
@@ -6,7 +6,10 @@ xml_doc = str(sys.argv[1])
 tree = ET.parse(xml_doc)
 root = tree.getroot()
 flight_information = {}
-
+#These are helpful links to finish this project:
+#https://docs.python.org/3/library/xml.etree.elementtree.html is my ElementTree documentation
+#https://anenadic.github.io/2014-11-10-manchester/novice/python/06-cmdline-non-interactive.html is running Python from the command line
+#https://www.datacamp.com/community/tutorials/dictionary-python?utm_source=adwords_ppc&utm_campaignid=1565261270&utm_adgroupid=67750485268&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=295208661514&utm_targetid=aud-299261629574:dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9009680&gclid=CjwKCAjw3pWDBhB3EiwAV1c5rCFSPniwB33l4PLFUr1_wJnwssiPVwmvpVDR_mxjAGd7P1dai_dbzRoC03sQAvD_BwE python dictionaries
 
 for child in root[0][0][1][0][0]:
     print(child.tag, child.attrib)
