08ae3f2f3c13893e26b9a2a4335642685c31cdc8
Katie Humburg <pookakat@gmail.com>
2021-04-03 21:09:41 -0400

    throw eggs at teflon walls

--- seatmap_parser.py
+++ seatmap_parser.py
@@ -5,6 +5,7 @@ import json
 xml_doc = str(sys.argv[1])
 tree = ET.parse(xml_doc)
 root = tree.getroot()
+
 #This section is going to make some assumptions in regards to scalability. The XML documents provided either come from IATA or #Opentravel, and the biggest assumption in regards to this is that IATA follows one format consistently, and Opentravel follows the #other. 
 #Here is where we'll find out which file is presented and how it will be handled.
 def strip_url_from_tag(child_info):
@@ -13,12 +14,22 @@ def strip_url_from_tag(child_info):
     url_information += "}"
     return url_information
 
+def get_date_time(date_time_dict):
+    date_time = str(date_time_dict.get('DepartureDateTime'))
+    return date_time
+
 def get_location_code(location_dict):
     airport_code = str(location_dict.get('LocationCode'))
     return airport_code
 
+def get_equipment(equipment_dict):
+    flight_equipment = str(equipment_dict.get('AirEquipType'))
+    return flight_equipment
 
 def ota_flight_handling(url_information):
+    for flight_departure_date_time in root.iter('{}FlightSegmentInfo'.format(url_information)):
+        flight_departure_date_time = get_date_time(flight_departure_date_time.attrib)
+
     for flight_departure_loc in root.iter('{}DepartureAirport'.format(url_information)):
         flight_departure_loc = get_location_code(flight_departure_loc.attrib)
 
@@ -26,14 +37,15 @@ def ota_flight_handling(url_information):
         flight_arrival_loc = get_location_code(flight_arrival_loc.attrib)
 
     for flight_equip_type in root.iter('{}Equipment'.format(url_information)):
-        flight_equip_type = (str(flight_equip_type.attrib))
+        flight_equip_type = (get_equipment(flight_equip_type.attrib))
 
+    print("Departure Date and Time: " + flight_departure_date_time)
     print("Flight Arrival: " + flight_arrival_loc)
     print("Flight Departure: " + flight_departure_loc)
     print("Flight Equipment: " + flight_equip_type)
 
 def iata_flight_handling(url_information):
-    print(str(root.iter('{}Departure[Time]'.format(url_information))))
+    print('No programming yet to handle {}. Please try again later'.format(url_information))
 
 for child in root[0]:
     if 'IATA' in child.tag:
@@ -48,28 +60,11 @@ for child in root[0]:
         print("Invalid File Type")
 
 
+
 #These are helpful links to finish this project:
 #https://docs.python.org/3/library/xml.etree.elementtree.html is my ElementTree documentation
 #https://anenadic.github.io/2014-11-10-manchester/novice/python/06-cmdline-non-interactive.html is running Python from the command line
 #https://www.datacamp.com/community/tutorials/dictionary-python?utm_source=adwords_ppc&utm_campaignid=1565261270&utm_adgroupid=67750485268&utm_device=c&utm_keyword=&utm_matchtype=b&utm_network=g&utm_adpostion=&utm_creative=295208661514&utm_targetid=aud-299261629574:dsa-429603003980&utm_loc_interest_ms=&utm_loc_physical_ms=9009680&gclid=CjwKCAjw3pWDBhB3EiwAV1c5rCFSPniwB33l4PLFUr1_wJnwssiPVwmvpVDR_mxjAGd7P1dai_dbzRoC03sQAvD_BwE python dictionaries
-class Plane:
-    def __init__(self, flight_departure_date_time, flight_arrival_loc, flight_departure_loc, flight_equip_type):
-        self.flight_departure_date_time = flight_departure_date_time
-        self.flight_arrival_loc = flight_arrival_loc
-        self.flight_departure_loc = flight_departure_loc
-        self.flight_equip_type = flight_equip_type
-
-class Row:
-    def __init__(self, row_class, row_seats):
-        self.row_class = row_class
-        self.row_seats = row_seats
-
-class Seats:
-    def __init__(self, seat_price, seat_availability, seat_type):
-        self.seat_price = seat_price
-        self.seat_availability = seat_availability
-        self.seat_type = seat_type
-
 
 #print(flight_information)
 #This is the basic layout for the command to get the JSON file. For now, we're working on how to get it in the first place. 
