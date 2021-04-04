c134cd56b95135e543090dda07eb84b8c30159f9
Katie Humburg <pookakat@gmail.com>
2021-04-04 17:17:45 -0400

    Figure out how to get everywhere in the xml

--- seatmap_parser.py
+++ seatmap_parser.py
@@ -8,6 +8,7 @@ root = tree.getroot()
 
 #This section is going to make some assumptions in regards to scalability. The XML documents provided either come from IATA or #Opentravel, and the biggest assumption in regards to this is that IATA follows one format consistently, and Opentravel follows the #other. 
 #Here is where we'll find out which file is presented and how it will be handled.
+row_numbers = []
 def strip_url_from_tag(child_info):
     entire_tag = str(child_info)
     url_information, unused_portion = entire_tag.split("}")
@@ -27,6 +28,7 @@ def get_equipment(equipment_dict):
     return flight_equipment
 
 def ota_flight_handling(url_information):
+    global row_numbers
     for flight_departure_date_time in root.iter('{}FlightSegmentInfo'.format(url_information)):
         flight_departure_date_time = get_date_time(flight_departure_date_time.attrib)
 
@@ -39,10 +41,22 @@ def ota_flight_handling(url_information):
     for flight_equip_type in root.iter('{}Equipment'.format(url_information)):
         flight_equip_type = (get_equipment(flight_equip_type.attrib))
 
+    for row_info in root.iter('{}RowInfo'.format(url_information)):
+        print(row_info.attrib)
+        for seat_info in row_info.iter('{}SeatInfo'.format(url_information)):
+            print(seat_info.attrib)
+            for seat in seat_info.iter('{}Summary'.format(url_information)):
+                print(seat.attrib)
+            for features in seat_info.iter('{}Features'.format(url_information)):
+                print(features.text)
+        
+
+
     print("Departure Date and Time: " + flight_departure_date_time)
     print("Flight Arrival: " + flight_arrival_loc)
     print("Flight Departure: " + flight_departure_loc)
     print("Flight Equipment: " + flight_equip_type)
+    print(row_numbers)
 
 def iata_flight_handling(url_information):
     print('No programming yet to handle {}. Please try again later'.format(url_information))
